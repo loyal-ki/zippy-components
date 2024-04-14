28a58a34df3c4ba1c3ebdff340b504370572c232
NghiaLMT <leminhtrungnghia8991@gmail.com>
2024-04-15 00:50:07 +0700

    feat: zippy components v.0.7.0

--- babel.config.js
+++ babel.config.js
@@ -8,6 +8,7 @@ module.exports = {
   presets: [
     'module:metro-react-native-babel-preset',
     '@babel/preset-typescript',
+    ['@babel/preset-env', { targets: { node: 'current' } }],
   ],
   plugins: [
     [
